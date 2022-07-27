420d07bc294ebae43d97e57a459a08685c3354f8
sagIoTPower <christof.strack@softwareag.com>
2022-07-27 08:54:34 +0200

    Removed unused classes

--- backend/src/main/java/mqttagent/configuration/MQTTConfiguration.java
+++ backend/src/main/java/mqttagent/configuration/MQTTConfiguration.java
@@ -1,63 +1,27 @@
 package mqttagent.configuration;
+import javax.validation.constraints.NotNull;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import lombok.Data;
 
-//@Component
-//@ConfigurationProperties("mqtt")
+@Data
 public class MQTTConfiguration {
-    // private String host;
-    // private int port;
-    // private String user;
-    // private String password;
-    // private String clientId;
-    // private Boolean useTLS;
 
-    // public String getHost() {
-    //     return host;
-    // }
+    @NotNull
+    public String mqttHost;
+    
+    @NotNull
+    public int mqttPort;
 
-    // public void setHost(String host) {
-    //     this.host = host;
-    // }
+    @NotNull
+    public String user;
 
-    // public int getPort() {
-    //     return port;
-    // }
+    @NotNull
+    public String password;
 
-    // public void setPort(int port) {
-    //     this.port = port;
-    // }
+    @NotNull
+    public String clientId;
 
-    // public String getUser() {
-    //     return user;
-    // }
-
-    // public void setUser(String user) {
-    //     this.user = user;
-    // }
-
-    // public String getPassword() {
-    //     return password;
-    // }
-
-    // public void setPassword(String password) {
-    //     this.password = password;
-    // }
-
-    // public String getClientId() {
-    //     return clientId;
-    // }
-
-    // public void setClientId(String clientId) {
-    //     this.clientId = clientId;
-    // }
-
-    // public Boolean getUseTLS() {
-    //     return useTLS;
-    // }
-
-    // public void setUseTLS(Boolean useTLS) {
-    //     this.useTLS = useTLS;
-    // }
+    @NotNull
+    public boolean useTLS;
 }
+
