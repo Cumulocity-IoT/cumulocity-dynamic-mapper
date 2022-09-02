18a7e700a16feb1c113f75173e52c9b4af1aa696
ck-sag <christof.strack@softwareag.com>
2022-09-02 19:08:28 +0200

    Delete setEnv.sh

--- frontend/mqtt-configuration/setEnv.sh
+++
@@ -1,5 +0,0 @@
-export user=christof.strack@softwareag.com
-export url=https://ck2.eu-latest.cumulocity.com
-#export url=https://nitrobox-swag.eu-latest.cumulocity.com
-#export url=https://novatec-gmbh.cumulocity.com/
-export password=#Manage250!DFC
