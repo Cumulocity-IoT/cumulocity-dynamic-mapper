1f18e83eb028a53d2b995d714416dac89cb46534
sagIoTPower <christof.strack@softwareag.com>
2022-09-13 13:19:45 +0200

    Added component to show selected mappings

---
+++ frontend/mqtt-configuration/src/mqtt-mapping/substitution/substitution-renderer.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Mapping } from 'src/mqtt-configuration.model';
+
+
+@Component({
+  selector: 'substitution-renderer',
+  templateUrl: 'substitution-renderer.component.html',
+  styleUrls: ['./substitution-renderer.style.css',
+  ],
+  encapsulation: ViewEncapsulation.None,
+})
+
+export class SubstitutionRendererComponent implements OnInit {
+
+  @Input()
+  substitutions: Mapping[] = [];
+
+/*   @Input()
+  color: string;
+
+  @Input()
+  selectedSubstitutionIndex: number = -1; */
+  @Input()
+  setting: {color : 'green', selectedSubstitutionIndex:1};
+
+  constructor() { }
+
+
+  ngOnInit() {
+    console.log ("Setting for renderer:", this.setting)
+
+  }
+
+}
