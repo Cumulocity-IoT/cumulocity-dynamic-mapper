08e7916a95ec5f5041f9cecc991cb8da3edd6397
sagIoTPower <christof.strack@softwareag.com>
2022-11-01 19:38:32 +0100

    changed interface of processcontext

--- backend/src/main/java/mqtt/mapping/processor/impl/FlatFileProcessor.java
+++ backend/src/main/java/mqtt/mapping/processor/impl/FlatFileProcessor.java
@@ -1,5 +1,6 @@
 package mqtt.mapping.processor.impl;
 
+import java.io.IOException;
 import java.nio.charset.Charset;
 
 import org.eclipse.paho.client.mqttv3.MqttMessage;
@@ -13,7 +14,7 @@ import mqtt.mapping.processor.ProcessingContext;
 import mqtt.mapping.service.MQTTClient;
 
 @Service
-public class FlatFileProcessor<I,O> extends JSONProcessor<String,String> {
+public class FlatFileProcessor<O> extends JSONProcessor<JsonNode> {
 
 
     public FlatFileProcessor( ObjectMapper objectMapper, MQTTClient mqttClient, C8yAgent c8yAgent){
@@ -21,16 +22,12 @@ public class FlatFileProcessor<I,O> extends JSONProcessor<String,String> {
     }
 
     @Override
-    public ProcessingContext<String> deserializePayload(ProcessingContext<String> context, MqttMessage mqttMessage) {
-        String payloadMessage = null;
-        if (mqttMessage.getPayload() != null) {
-            payloadMessage = (mqttMessage.getPayload() != null
+    public ProcessingContext<JsonNode> deserializePayload(ProcessingContext<JsonNode> context, MqttMessage mqttMessage) throws IOException {
+        String payloadMessage  = (mqttMessage.getPayload() != null
                     ? new String(mqttMessage.getPayload(), Charset.defaultCharset())
                     : "");
-            JsonNode payloadJsonNode = objectMapper.valueToTree(new PayloadWrapper(payloadMessage));
-            payloadMessage = payloadJsonNode.toString();
-        }
-        context.setPayload(payloadMessage);
+        JsonNode payloadJsonNode = objectMapper.valueToTree(new PayloadWrapper(payloadMessage));
+        context.setPayload(payloadJsonNode);
         return context;
     }
 
--- backend/src/main/java/mqtt/mapping/processor/impl/GenericBinaryProcessor.java
+++ backend/src/main/java/mqtt/mapping/processor/impl/GenericBinaryProcessor.java
@@ -1,5 +1,7 @@
 package mqtt.mapping.processor.impl;
 
+import java.io.IOException;
+
 import org.apache.commons.codec.binary.Hex;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.stereotype.Service;
@@ -12,21 +14,16 @@ import mqtt.mapping.processor.ProcessingContext;
 import mqtt.mapping.service.MQTTClient;
 
 @Service
-public class GenericBinaryProcessor<I,O> extends JSONProcessor<byte[], String> {
+public class GenericBinaryProcessor<O> extends JSONProcessor<JsonNode> {
 
     public GenericBinaryProcessor ( ObjectMapper objectMapper, MQTTClient mqttClient, C8yAgent c8yAgent){
         super(objectMapper, mqttClient, c8yAgent);
     }
 
     @Override
-    public ProcessingContext<String> deserializePayload(ProcessingContext<String> context, MqttMessage mqttMessage) {
-        String payloadMessage = null;
-        if (mqttMessage.getPayload() != null) {
-            JsonNode payloadJsonNode = objectMapper.valueToTree(new PayloadWrapper(Hex.encodeHexString(mqttMessage.getPayload())));
-            payloadMessage = payloadJsonNode.toString();
-        }
-        context.setPayload(payloadMessage);
+    public ProcessingContext<JsonNode> deserializePayload(ProcessingContext<JsonNode> context, MqttMessage mqttMessage) throws IOException{
+        JsonNode payloadJsonNode = objectMapper.valueToTree(new PayloadWrapper(Hex.encodeHexString(mqttMessage.getPayload())));
+        context.setPayload(payloadJsonNode);
         return context;
     }
-
 }
