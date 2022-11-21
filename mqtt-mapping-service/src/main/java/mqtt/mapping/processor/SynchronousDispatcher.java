58fc5c0c3222df0658167e3a1b3c5ad99201e3c6
sagIoTPower <christof.strack@softwareag.com>
2022-11-21 09:33:07 +0100

    update handler

--- mqtt-mapping-service/src/main/java/mqtt/mapping/processor/SynchronousDispatcher.java
+++ mqtt-mapping-service/src/main/java/mqtt/mapping/processor/SynchronousDispatcher.java
@@ -1,158 +1,163 @@
-// package mqtt.mapping.processor;
+package mqtt.mapping.processor;
 
-// import java.util.ArrayList;
-// import java.util.List;
-// import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-// import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
-// import org.eclipse.paho.client.mqttv3.MqttCallback;
-// import org.eclipse.paho.client.mqttv3.MqttMessage;
-// import org.joda.time.DateTime;
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.joda.time.DateTime;
+import org.springframework.beans.factory.annotation.Autowired;
 
-// import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 
-// import lombok.extern.slf4j.Slf4j;
-// import mqtt.mapping.core.C8YAgent;
-// import mqtt.mapping.model.Mapping;
-// import mqtt.mapping.model.MappingStatus;
-// import mqtt.mapping.model.SnoopStatus;
-// import mqtt.mapping.processor.extension.ExtensibleProcessor;
-// import mqtt.mapping.processor.extension.ProcessorExtension;
-// import mqtt.mapping.processor.model.C8YRequest;
-// import mqtt.mapping.processor.model.MappingType;
-// import mqtt.mapping.processor.model.ProcessingContext;
-// import mqtt.mapping.processor.system.SysHandler;
-// import mqtt.mapping.service.MQTTClient;
-// import mqtt.mapping.util.SpringUtil;
+import lombok.extern.slf4j.Slf4j;
+import mqtt.mapping.configuration.ServiceConfigurationComponent;
+import mqtt.mapping.core.C8YAgent;
+import mqtt.mapping.core.MappingComponent;
+import mqtt.mapping.model.Mapping;
+import mqtt.mapping.model.MappingStatus;
+import mqtt.mapping.model.SnoopStatus;
+import mqtt.mapping.processor.extension.ExtensibleProcessor;
+import mqtt.mapping.processor.extension.ProcessorExtension;
+import mqtt.mapping.processor.model.C8YRequest;
+import mqtt.mapping.processor.model.MappingType;
+import mqtt.mapping.processor.model.ProcessingContext;
+import mqtt.mapping.processor.system.SysHandler;
+import mqtt.mapping.service.MQTTClient;
+import mqtt.mapping.util.SpringUtil;
 
-// @Slf4j
-// @Service
-// public class SynchronousDispatcher implements MqttCallback {
+@Slf4j
+@Service
+public class SynchronousDispatcher implements MqttCallback {
 
-//     private static final Object TOPIC_PERFORMANCE_METRIC = "__TOPIC_PERFORMANCE_METRIC";
+    private static final Object TOPIC_PERFORMANCE_METRIC = "__TOPIC_PERFORMANCE_METRIC";
 
-//     @Autowired
-//     protected C8YAgent c8yAgent;
+    @Autowired
+    protected C8YAgent c8yAgent;
 
-//     @Autowired
-//     protected MQTTClient mqttClient;
+    @Autowired
+    protected MQTTClient mqttClient;
 
-//     @Autowired
-//     ExtensibleProcessor<?> extensionPayloadProcessor;
+    @Autowired
+    ExtensibleProcessor<?> extensionPayloadProcessor;
 
-//     @Autowired
-//     SysHandler sysHandler;
+    @Autowired
+    SysHandler sysHandler;
 
-//     @Autowired
-//     Map<MappingType, BasePayloadProcessor<?>> payloadProcessors;
+    @Autowired
+    Map<MappingType, BasePayloadProcessor<?>> payloadProcessors;
 
-//     @Autowired
-//     private SpringUtil springUtil;
+    
+    @Autowired
+    MappingComponent mappingStatusComponent;
 
-//     public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
-//         if ((TOPIC_PERFORMANCE_METRIC.equals(topic))) {
-//             // REPORT MAINTENANCE METRIC
-//         } else {
-//             processMessage(topic, mqttMessage, true);
-//         }
-//     }
+    @Autowired
+    ServiceConfigurationComponent serviceConfigurationComponent;
 
-//     public List<ProcessingContext<?>> processMessage(String topic, MqttMessage mqttMessage, boolean sendPayload)
-//             throws Exception {
-//         MappingStatus mappingStatusUnspecified = mqttClient.getMappingStatus(null, true);
-//         List<ProcessingContext<?>> processingResult = new ArrayList<ProcessingContext<?>>();
+    public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+        if ((TOPIC_PERFORMANCE_METRIC.equals(topic))) {
+            // REPORT MAINTENANCE METRIC
+        } else {
+            processMessage(topic, mqttMessage, true);
+        }
+    }
 
-//         if (topic != null && !topic.startsWith("$SYS")) {
-//             if (mqttMessage.getPayload() != null) {
-//                 List<Mapping> resolvedMappings = new ArrayList<>();
-//                 try {
-//                     resolvedMappings = mqttClient.resolveMappings(topic);
-//                 } catch (Exception e) {
-//                     log.warn("Error resolving appropriate map. Could NOT be parsed. Ignoring this message!", e);
-//                     mappingStatusUnspecified.errors++;
-//                     // TODO review if exception has to be thrown
-//                     // throw e;
-//                 }
+    public List<ProcessingContext<?>> processMessage(String topic, MqttMessage mqttMessage, boolean sendPayload)
+            throws Exception {
+        MappingStatus mappingStatusUnspecified = mappingStatusComponent.getMappingStatus(null, true);
+        List<ProcessingContext<?>> processingResult = new ArrayList<ProcessingContext<?>>();
 
-//                 resolvedMappings.forEach(mapping -> {
-//                     MappingStatus mappingStatus = mqttClient.getMappingStatus(mapping, false);
+        if (topic != null && !topic.startsWith("$SYS")) {
+            if (mqttMessage.getPayload() != null) {
+                List<Mapping> resolvedMappings = new ArrayList<>();
+                try {
+                    resolvedMappings = mqttClient.resolveMappings(topic);
+                } catch (Exception e) {
+                    log.warn("Error resolving appropriate map. Could NOT be parsed. Ignoring this message!", e);
+                    mappingStatusUnspecified.errors++;
+                    // TODO review if exception has to be thrown
+                    // throw e;
+                }
 
-//                     ProcessingContext<?> context;
-//                     if (mapping.mappingType.payloadType.equals(String.class)) {
-//                         context = new ProcessingContext<String>();
-//                     } else {
-//                         context = new ProcessingContext<byte[]>();
+                resolvedMappings.forEach(mapping -> {
+                    MappingStatus mappingStatus = mappingStatusComponent.getMappingStatus(mapping, false);
 
-//                     }
-//                     context.setTopic(topic);
-//                     context.setMappingType(mapping.mappingType);
-//                     context.setMapping(mapping);
-//                     context.setSendPayload(sendPayload);
-//                     // identify the corect processor based on the mapping type
-//                     MappingType mappingType = context.getMappingType();
-//                     BasePayloadProcessor processor = payloadProcessors.get(mappingType);
+                    ProcessingContext<?> context;
+                    if (mapping.mappingType.payloadType.equals(String.class)) {
+                        context = new ProcessingContext<String>();
+                    } else {
+                        context = new ProcessingContext<byte[]>();
 
-//                     if (processor != null) {
-//                         try {
-//                             processor.deserializePayload(context, mqttMessage);
-//                             if (mqttClient.getServiceConfiguration().logPayload) {
-//                                 log.info("New message on topic: '{}', wrapped message: {}", context.getTopic(),
-//                                         context.getPayload().toString());
-//                             } else {
-//                                 log.info("New message on topic: '{}'", context.getTopic());
-//                             }
-//                             mappingStatus.messagesReceived++;
-//                             if (mapping.snoopStatus == SnoopStatus.ENABLED
-//                                     || mapping.snoopStatus == SnoopStatus.STARTED) {
-//                                 if (context.getPayload() instanceof String) {
-//                                     String payload = (String) context.getPayload();
-//                                     mappingStatus.snoopedTemplatesActive++;
-//                                     mappingStatus.snoopedTemplatesTotal = mapping.snoopedTemplates.size();
-//                                     mapping.addSnoopedTemplate(payload);
+                    }
+                    context.setTopic(topic);
+                    context.setMappingType(mapping.mappingType);
+                    context.setMapping(mapping);
+                    context.setSendPayload(sendPayload);
+                    // identify the corect processor based on the mapping type
+                    MappingType mappingType = context.getMappingType();
+                    BasePayloadProcessor processor = payloadProcessors.get(mappingType);
 
-//                                     log.debug("Adding snoopedTemplate to map: {},{},{}", mapping.subscriptionTopic,
-//                                             mapping.snoopedTemplates.size(),
-//                                             mapping.snoopStatus);
-//                                     mqttClient.setMappingDirty(mapping);
+                    if (processor != null) {
+                        try {
+                            processor.deserializePayload(context, mqttMessage);
+                            if (mqttClient.getServiceConfiguration().logPayload) {
+                                log.info("New message on topic: '{}', wrapped message: {}", context.getTopic(),
+                                        context.getPayload().toString());
+                            } else {
+                                log.info("New message on topic: '{}'", context.getTopic());
+                            }
+                            mappingStatus.messagesReceived++;
+                            if (mapping.snoopStatus == SnoopStatus.ENABLED
+                                    || mapping.snoopStatus == SnoopStatus.STARTED) {
+                                if (context.getPayload() instanceof String) {
+                                    String payload = (String) context.getPayload();
+                                    mappingStatus.snoopedTemplatesActive++;
+                                    mappingStatus.snoopedTemplatesTotal = mapping.snoopedTemplates.size();
+                                    mapping.addSnoopedTemplate(payload);
 
-//                                 }
-//                             } else {
-//                                 processor.extractFromSource(context);
-//                                 processor.substituteInTargetAndSend(context);
-//                                 ArrayList<C8YRequest> resultRequests = context.getRequests();
-//                                 if (context.hasError() || resultRequests.stream().anyMatch(r -> r.hasError())) {
-//                                     mappingStatus.errors++;
-//                                 }
-//                             }
-//                         } catch (Exception e) {
-//                             log.warn("Message could NOT be parsed, ignoring this message.");
-//                             e.printStackTrace();
-//                             mappingStatus.errors++;
-//                         }
-//                     } else {
-//                         mappingStatusUnspecified.errors++;
-//                         log.error("No process for MessageType: {} registered, ignoring this message!", mappingType);
-//                     }
-//                     processingResult.add(context);
-//                 });
-//             }
-//         } else {
-//             sysHandler.handleSysPayload(topic, mqttMessage);
-//         }
+                                    log.debug("Adding snoopedTemplate to map: {},{},{}", mapping.subscriptionTopic,
+                                            mapping.snoopedTemplates.size(),
+                                            mapping.snoopStatus);
+                                    mappingStatusComponent.setMappingDirty(mapping);
+                                }
+                            } else {
+                                processor.extractFromSource(context);
+                                processor.substituteInTargetAndSend(context);
+                                ArrayList<C8YRequest> resultRequests = context.getRequests();
+                                if (context.hasError() || resultRequests.stream().anyMatch(r -> r.hasError())) {
+                                    mappingStatus.errors++;
+                                }
+                            }
+                        } catch (Exception e) {
+                            log.warn("Message could NOT be parsed, ignoring this message.");
+                            e.printStackTrace();
+                            mappingStatus.errors++;
+                        }
+                    } else {
+                        mappingStatusUnspecified.errors++;
+                        log.error("No process for MessageType: {} registered, ignoring this message!", mappingType);
+                    }
+                    processingResult.add(context);
+                });
+            }
+        } else {
+            sysHandler.handleSysPayload(topic, mqttMessage);
+        }
 
-//         return processingResult;
-//     }
+        return processingResult;
+    }
 
-//     @Override
-//     public void connectionLost(Throwable throwable) {
-//         log.error("Connection Lost to MQTT broker: ", throwable);
-//         c8yAgent.createEvent("Connection lost to MQTT broker", "mqtt_status_event", DateTime.now(), null);
-//         mqttClient.submitConnect();
-//     }
+    @Override
+    public void connectionLost(Throwable throwable) {
+        log.error("Connection Lost to MQTT broker: ", throwable);
+        c8yAgent.createEvent("Connection lost to MQTT broker", "mqtt_status_event", DateTime.now(), null);
+        mqttClient.submitConnect();
+    }
 
-//     @Override
-//     public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-//     }
+    @Override
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+    }
 
-// }
+}
