e2675ea6a535f86d86af15f388df19fbeed6e639
Stefan Witschel <Stefan.Witschel@softwareag.com>
2023-02-06 19:08:53 +0100

    Added PUT Endpoint to CRUD multiple subscriptions

--- mqtt-mapping-service/src/main/java/mqtt/mapping/rest/MQTTOutgoingMappingRestController.java
+++ mqtt-mapping-service/src/main/java/mqtt/mapping/rest/MQTTOutgoingMappingRestController.java
@@ -17,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @RestController
@@ -40,6 +41,66 @@ public class MQTTOutgoingMappingRestController {
                     //throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Managed Object with id "+device.getId()+ " not found" );
                 }
             }
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
+        }
+        return ResponseEntity.ok().build();
+    }
+
+    @RequestMapping(value = "/subscription", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> subscriptionUpdate(@Valid @RequestBody C8YAPISubscription subscription) {
+        try {
+            List<NotificationSubscriptionRepresentation> deviceSubscriptions = c8yApiSubscriber.getDeviceSubscriptions(null, null).get();
+            //3 cases -
+            // 1. Device exists in subscription and active subscription --> Do nothing
+            // 2. Device exists in subscription and does not have an active subscription --> create subscription
+            // 3. Device exists not in subscription and does have an active subscription --> delete subscription
+            deviceSubscriptions.stream().forEach( deviceSubscription -> {
+                String deviceId = deviceSubscription.getSource().getId().getValue();
+                boolean subInDeviceList = false;
+
+                for (Device device : subscription.getDevices()) {
+                    if (deviceId.equals(device.getId())) {
+                        //1. Subscription exists - do nothing
+                        subInDeviceList = true;
+                        break;
+                    }
+                }
+                if (!subInDeviceList) {
+                    //3. delete Subscription
+                    ManagedObjectRepresentation mor = c8yAgent.getManagedObjectForId(deviceSubscription.getSource().getId().getValue());
+                    if (mor != null) {
+                        c8yApiSubscriber.unsubscribeDevice(mor, null);
+                    } else {
+                        log.warn("Could not delete subscription for device with id "+deviceId+ ". Device not found" );
+                    }
+                }
+            });
+            subscription.getDevices().stream().forEach(device -> {
+                boolean deviceInSubList = false;
+                for (NotificationSubscriptionRepresentation sub : deviceSubscriptions) {
+                    if (sub.getSource().getId().getValue().equals(device.getId())) {
+                        //1. Subscription exists - do nothing
+                        deviceInSubList = true;
+                        break;
+                    }
+                }
+                if (!deviceInSubList) {
+                    //2. create subscription
+                    ManagedObjectRepresentation mor = c8yAgent.getManagedObjectForId(device.getId());
+                    if (mor != null) {
+                        try {
+                            c8yApiSubscriber.subscribeDevice(mor, subscription.getApi());
+                        } catch (Exception e) {
+                            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
+                        }
+                    } else {
+                        log.warn("Could not subscribe device with id "+device.getId()+ ". Device does not exists!" );
+                    }
+                }
+
+            });
+
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getLocalizedMessage());
         }
