6a34c0a147fe2770025abbf00366a592e61e8576
sagIoTPower <christof.strack@softwareag.com>
2023-07-12 11:43:46 +0200

    del missin key for MappingType.PROCESSOR_EXTENSION

--- mqtt-mapping-service/src/main/java/mqtt/mapping/processor/outbound/BasePayloadProcessorOutbound.java
+++ mqtt-mapping-service/src/main/java/mqtt/mapping/processor/outbound/BasePayloadProcessorOutbound.java
@@ -38,6 +38,7 @@ import mqtt.mapping.model.MappingSubstitution.SubstituteValue.TYPE;
 import mqtt.mapping.processor.C8YMessage;
 import mqtt.mapping.processor.ProcessingException;
 import mqtt.mapping.processor.model.C8YRequest;
+import mqtt.mapping.processor.model.MappingType;
 import mqtt.mapping.processor.model.ProcessingContext;
 import mqtt.mapping.processor.model.RepairStrategy;
 import mqtt.mapping.processor.system.SysHandler;
@@ -123,9 +124,9 @@ public abstract class BasePayloadProcessorOutbound<T> {
                         deviceSource = externalId.getExternalId();
                     }
                 }
-                substituteValueInObject(substituteValue, payloadTarget, pathTarget);
+                substituteValueInObject(mapping.mappingType, substituteValue, payloadTarget, pathTarget);
             } else if (!pathTarget.equals(MappingRepresentation.findDeviceIdentifier(mapping).pathTarget)) {
-                substituteValueInObject(substituteValue, payloadTarget, pathTarget);
+                substituteValueInObject(mapping.mappingType, substituteValue, payloadTarget, pathTarget);
             }
         }
         /*
@@ -181,12 +182,14 @@ public abstract class BasePayloadProcessorOutbound<T> {
 
     }
 
-    public void substituteValueInObject(SubstituteValue sub, DocumentContext jsonObject, String keys)
+    public void substituteValueInObject(MappingType type, SubstituteValue sub, DocumentContext jsonObject, String keys)
             throws JSONException {
         boolean subValueMissing = sub.value == null;
         boolean subValueNull = sub.value.isNull();
-        if ((sub.repairStrategy.equals(RepairStrategy.REMOVE_IF_MISSING) && subValueMissing)
-                || (sub.repairStrategy.equals(RepairStrategy.REMOVE_IF_NULL) && subValueNull)) {
+        if ((sub.repairStrategy.equals(RepairStrategy.REMOVE_IF_MISSING) && subValueMissing) ||
+                (sub.repairStrategy.equals(RepairStrategy.REMOVE_IF_NULL) && subValueNull) ||
+                ((type.equals(MappingType.PROCESSOR_EXTENSION) || type.equals(MappingType.PROTOBUF_STATIC))
+                        && (subValueMissing || subValueNull))) {
             jsonObject.delete(keys);
         } else {
             jsonObject.set(keys, sub.typedValue());
