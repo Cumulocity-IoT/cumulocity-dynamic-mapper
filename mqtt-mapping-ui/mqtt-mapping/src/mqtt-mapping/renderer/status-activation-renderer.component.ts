278d285dec9ebd57bd095bace90ae638084e29c5
sagIoTPower <christof.strack@softwareag.com>
2023-10-12 18:22:42 +0200

    removing button in row of mapping for activation

---
+++ mqtt-mapping-ui/mqtt-mapping/src/mqtt-mapping/renderer/status-activation-renderer.component.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Software AG, Darmstadt, Germany and/or Software AG USA Inc., Reston, VA, USA,
+ * and/or its subsidiaries and/or its affiliates and/or their licensors.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @authors Christof Strack
+ */
+import { Component, Inject, ViewEncapsulation } from "@angular/core";
+import { CellRendererContext } from "@c8y/ngx-components";
+
+/**
+ * The example component for custom cell renderer.
+ * It gets `context` with the current row item and the column.
+ * Additionally, a service is injected to provide a helper method.
+ * The template displays the icon and the label with additional styling.
+ */
+@Component({
+  encapsulation: ViewEncapsulation.None,
+  template: `
+    <span [title]="context.item.active">
+      <i style= "text-align:center; width: 100%;" [c8yIcon]="!context.value ? 'circle-o' : 'plus-circle-o'" class="m-r-5"></i>
+    </span>
+  `,
+})
+export class StatusActivationRendererComponent {
+  constructor(
+    public context: CellRendererContext,
+  ) {
+    //console.log("Status", context, context.value)
+  }
+}
