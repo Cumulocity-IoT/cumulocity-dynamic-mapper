881141fdc037aff4f048fa02411524916178d168
sagIoTPower <christof.strack@softwareag.com>
2023-12-29 10:47:34 +0100

    fixed bugs in build script

--- dynamic-mapping-service/src/main/java/dynamic/mapping/processor/extension/external/package-info.java
+++ dynamic-mapping-service/src/main/java/dynamic/mapping/processor/extension/external/package-info.java
@@ -1,10 +1,10 @@
 /**
  * This module holds all the externally loaded processor extenions.
  * External processor extensions have to:
- * 1. implement the inteface <code>ProcessorExtensionInbound<O></code> 
+ * 1. implement the interface <code>ProcessorExtensionInbound<O></code>
  * 2. be registered in the properties file <code>/mqtt-mapping-extension/src/main/resources/extension-external.properties</code>
  * 3. be developed/packed in the maven module <code>/mqtt-mapping-extension</code>. NOT in this maven module.
- * 4. be uploaded throught the Web UI
+ * 4. be uploaded through the Web UI
  * <p>
 
  * </p>
