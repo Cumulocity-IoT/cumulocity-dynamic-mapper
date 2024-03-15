d5b39e17318253cbf2dbc4b3dc3a73e795947570
sagIoTPower <christof.strack@softwareag.com>
2024-03-15 15:13:48 +0100

    changed client

--- dynamic-mapping-service/src/main/java/dynamic/mapping/connector/core/callback/GenericMessageCallback.java
+++ dynamic-mapping-service/src/main/java/dynamic/mapping/connector/core/callback/GenericMessageCallback.java
@@ -3,7 +3,7 @@ package dynamic.mapping.connector.core.callback;
 public interface GenericMessageCallback {
     void onClose(String closeMessage, Throwable closeException);
 
-    void onMessage(ConnectorMessage message);
+    void onMessage(ConnectorMessage message) throws Exception;
 
     void onError( Throwable errorException);
 }
