54f59b4192878ffceeffdc8233627eeaba69abab
ck-c8y <christof.strack@cumulocity.com>
2024-12-05 17:19:32 +0100

    add migtation scripts

--- resources/script/migrate/clean_mappings.sh
+++ resources/script/migrate/clean_mappings.sh
@@ -5,4 +5,4 @@ set -e
 # script to delete mappings
 # this can be used after a migration to a new version of the dynamic mapper. in case the structure of the mappings has changed and become invalid
 ### 
-c8y inventory list --type d11r_mapping | c8y inventory delete
+c8y inventory list --type d11r_mapping --includeAll | c8y inventory delete
--- resources/script/migrate/migrate_mappings_46_47.sh
+++ resources/script/migrate/migrate_mappings_46_47.sh
@@ -16,13 +16,13 @@ set -e
 ###
 
 ORIGINAL_MAPPINGS="mappings-v46x.json"
-_ORIGINAL_MAPPINGS="_mappings-v46x.json"
 MIGRATED_MAPPINGS="mappings-v47x.json"
 
-# step 1 save existing mappings to file ORIGINAL_MAPPINGS
-c8y inventory list --type d11r_mapping --includeAll | jq -s '.' >$ORIGINAL_MAPPINGS
+# step 1 save existing mappings to file ORIGINAL_MAPPINGS and only use the attribute d11r_mapping
+c8y inventory list --type d11r_mapping --includeAll | jq -s 'map(.d11r_mapping)' >$ORIGINAL_MAPPINGS
+
 # step 2 transform mappings to the new format
-cat $_ORIGINAL_MAPPINGS | jq '[.[] | 
+cat $ORIGINAL_MAPPINGS | jq '[.[] | 
   . + {
     sourceTemplate: .source,
     targetTemplate: .target
@@ -33,13 +33,18 @@ cat $_ORIGINAL_MAPPINGS | jq '[.[] |
           eventName: .extension.event,
           extensionName: .extension.name,
           extensionType: "PROCESSOR_EXTENSION_SOURCE"
-        } | del(.event, .name, .loaded))
+        } | del(.event, .name, .loaded)),
+       "mappingType": "PROCESSOR_EXTENSION_SOURCE",
       } else {} end)
    | del(.source, .target, .filterOutbound, .subscriptionTopic)
 ]' >$MIGRATED_MAPPINGS
 
 # step 3 delete old mappings
-c8y inventory list --type d11r_mapping | c8y inventory delete
+c8y inventory list --type d11r_mapping --includeAll | c8y inventory delete 
 
 # step 4 create transformed mappings
-cat $MIGRATED_MAPPINGS | jq -c '.[]'| c8y inventory create 
+cat $MIGRATED_MAPPINGS | jq -c 'to_entries[] | {
+  name: .value.name,
+  type: "d11r_mapping",
+  d11r_mapping: .value
+}'| c8y inventory create --template "input.value"
