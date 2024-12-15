aeff15a2e236564a7a03dd4a3f8b8278f9ab4130
ck-c8y <christof.strack@cumulocity.com>
2024-12-15 14:41:28 +0100

    update migration script

--- resources/script/mgmt/dynamic-mapping-mgmt.sh
+++ resources/script/mgmt/dynamic-mapping-mgmt.sh
@@ -29,13 +29,45 @@ ORIGINAL_MAPPINGS_NAME="mappings-v46"
 MIGRATED_MAPPINGS_name="mappings-v47"
 
 function show_usage() {
-  echo "Usage: $0 [migrateMappings|deleteMappings|deleteConnectors]"
-  echo "  migrateMappings: Create network and volumes"
-  echo "  deleteMappings: Delete mappings"
-  echo "  deleteConnectors: Delete connectos"
+  echo "Usage: $0 [migrateMappings|deleteMappings|deleteConnectors|importMappingsAsMO|exportMappingsAsMO]"
+  echo "  migrateMappings:     Migrate mappings from pre 4.7 format to new format"
+  echo "  deleteMappings:      Delete mappings"
+  echo "  deleteConnectors:    Delete connectors"
+  echo "  importMappingsAsMO   [filename]: Import mappings from file as managedObjects"
+  echo "  exportMappingsAsMO:  [filename]: Export mappings to file as managedObjects"
+  echo ""
+  echo "If filename is not provided, defaults to 'mappings-all.json'"
+
+  check_prerequisites
+}
+
+function import_mappings_as_mo() {
+  local missing_programs=()
+  local filename="${1:-mappings-all.json}" # Default to mappings-all.json if no argument provided
+
+  if [ ! -f "$filename" ]; then
+    echo "Error: File '$filename' not found!"
+    exit 1
+  fi
+
+  cat "$filename" | jq -c -n '
+  [ inputs ] | to_entries[] | {
+    name: ("Mapping - " + ((.key + 1) | tostring)),
+    type: "d11r_mapping",
+    d11r_mapping: .value.d11r_mapping
+  }
+'  | c8y inventory create --template "input.value"
+}
+
+function export_mappings_as_mo() {
+  local missing_programs=()
+  local filename="${1:-mappings-all.json}" # Default to mappings-all.json if no argument provided
+
+    c8y inventory list --type d11r_mapping --includeAll --select name,type,d11r_mapping > "$filename" 
 }
 
 function migrate_mappings() {
+  check_prerequisites
   # step 0 save existing mappings to file ORIGINAL_MAPPINGS and only use the attribute d11r_mapping
   echo 'step 0 save existing mappings to file ORIGINAL_MAPPINGS and only use the attribute d11r_mapping'
   c8y inventory list --type d11r_mapping --includeAll | jq -s 'map(.d11r_mapping)' >${ORIGINAL_MAPPINGS_NAME}.json
@@ -117,13 +149,37 @@ function migrate_mappings() {
 }
 
 function delete_mappings() {
+  check_prerequisites
   echo 'delete mappings'
   c8y inventory list --includeAll --type d11r_mapping | c8y inventory delete
 }
 
 function delete_connectors() {
+  check_prerequisites
   echo 'delete connectors'
   c8y tenantoptions getForCategory --category dynamic.mapper.service | jq 'keys| .[] | select(startswith("credentials.connection.configuration"))' -r | c8y tenantoptions delete --category dynamic.mapper.service --key -.key
+}
+
+function check_prerequisites() {
+  local missing_programs=()
+
+  # Check for jq
+  if ! command -v jq >/dev/null 2>&1; then
+    missing_programs+=("jq")
+  fi
+
+  # Check for c8y
+  if ! command -v c8y >/dev/null 2>&1; then
+    missing_programs+=("c8y")
+  fi
+
+  # If any programs are missing, print error message and exit
+  if [ ${#missing_programs[@]} -ne 0 ]; then
+    echo "Error: Required programs are not installed:"
+    printf '%s\n' "${missing_programs[@]}"
+    echo "Please install the missing programs and try again, for go c8y cli see https://goc8ycli.netlify.app/"
+    exit 1
+  fi
 }
 
 # Check if argument is provided
@@ -142,6 +198,12 @@ deleteMappings)
   ;;
 deleteConnectors)
   delete_connectors
+  ;;
+importMappingsAsMO)
+  import_mappings_as_mo "${2:-}"
+  ;;
+exportMappingsAsMO)
+  export_mappings_as_mo "${2:-}"
   ;;
 *)
   show_usage
