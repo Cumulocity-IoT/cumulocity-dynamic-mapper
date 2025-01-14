f181dd63dbce878f8a62650c16748ba359de0060
ck-c8y <christof.strack@cumulocity.com>
2025-01-14 16:28:02 +0100

    introduce splitTopicExcludingSeparatorIncludingLeagingSlash to incluse first leading slash

--- dynamic-mapping-ui/src/mapping/shared/util.ts
+++ dynamic-mapping-ui/src/mapping/shared/util.ts
@@ -34,13 +34,13 @@ export const TOKEN_CONTEXT_DATA = '_CONTEXT_DATA_';
 export const CONTEXT_DATA_KEY_NAME = 'key';
 export const TIME = 'time';
 
-// export function splitTopicExcludingSeparator(topic: string): string[] {
-//   let topix = topic;
-//   topix = topix.trim().replace(/(\/{1,}$)|(^\/{1,})/g, '');
-//   return topix.split(/\//g);
-// }
-
 export function splitTopicExcludingSeparator(topic: string): string[] {
+  let topix = topic;
+  topix = topix.trim().replace(/(\/{1,}$)|(^\/{1,})/g, '');
+  return topix.split(/\//g);
+}
+
+export function splitTopicExcludingSeparatorIncludingLeagingSlash(topic: string): string[] {
   let topix = topic;
   topix = topix.trim().replace(/(\/{1,}$)/g, ''); // Remove trailing slashes
   if (topix.startsWith('//')) {                    // If there are multiple leading slashes
