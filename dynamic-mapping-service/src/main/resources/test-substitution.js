600034db6312782450088d481088eb7f15413d58
ck-c8y <christof.strack@cumulocity.com>
2025-02-20 16:00:05 +0100

    update dependencies

--- dynamic-mapping-service/src/main/resources/test-substitution.js
+++ dynamic-mapping-service/src/main/resources/test-substitution.js
@@ -1,6 +1,4 @@
-const Java = Packages.java;
 const SubstitutionResult = Java.type('dynamic.mapping.processor.extension.internal.SubstitutionResult');
-
 function map(ctx) {
     return new SubstitutionResult({
         myNameIs: ctx.getName(),
