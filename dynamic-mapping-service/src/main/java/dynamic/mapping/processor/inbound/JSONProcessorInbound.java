89eb9cfa6b83fcf3d17231022f64d9c584834180
switschel <stefan.witschel@cumulocity.com>
2025-03-10 15:44:15 +0100

    Log message when inbound filter applies

--- dynamic-mapping-service/src/main/java/dynamic/mapping/processor/inbound/JSONProcessorInbound.java
+++ dynamic-mapping-service/src/main/java/dynamic/mapping/processor/inbound/JSONProcessorInbound.java
@@ -145,7 +145,7 @@ public class JSONProcessorInbound extends BaseProcessorInbound<Object> {
             try {
                 var expr = jsonata(mappingFilter);
                 Object extractedSourceContent = expr.evaluate(payloadObjectNode);
-                log.info("Tenant {} - Payload will be ignored due to filter: {}, {}", tenant, extractedSourceContent, payload);
+                log.info("Tenant {} - Payload will be ignored due to filter: {}, {}", tenant, mappingFilter, payload);
                 context.setIgnoreFurtherProcessing(!isNodeTrue(extractedSourceContent));
             } catch (Exception e) {
                 log.error("Tenant {} - Exception for: {}, {}: ", tenant, mappingFilter,
