24b6f0c7170e3ba93c63805e1b3e089328c36617
ck-c8y <christof.strack@cumulocity.com>
2025-04-18 21:51:26 +0200

    avoid servers

--- dynamic-mapping-service/src/main/java/dynamic/mapping/core/OpenAPIConfig.java
+++ dynamic-mapping-service/src/main/java/dynamic/mapping/core/OpenAPIConfig.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,21 +22,23 @@ public class OpenAPIConfig {
         contact.setUrl("https://www.cumulocity.com");
 
         License mitLicense = new License()
-            .name(" Apache License")
-            .url("https://raw.githubusercontent.com/Cumulocity-IoT/cumulocity-dynamic-mapper/refs/heads/main/LICENSE");
+                .name(" Apache License")
+                .url("https://raw.githubusercontent.com/Cumulocity-IoT/cumulocity-dynamic-mapper/refs/heads/main/LICENSE");
 
         Info info = new Info()
-            .title("Cumulocity Dynamic Mapper")
-            .version("5.0.0")
-            .contact(contact)
-            .description("This API exposes endpoints to manage resources for the Cumulocity Dynamic Mapper.")
-            .termsOfService("https://raw.githubusercontent.com/Cumulocity-IoT/cumulocity-dynamic-mapper/refs/heads/main/LICENSE")
-            .license(mitLicense);
+                .title("Cumulocity Dynamic Mapper")
+                .version("5.0.0")
+                .contact(contact)
+                .description("This API exposes endpoints to manage resources for the Cumulocity Dynamic Mapper.")
+                .termsOfService(
+                        "https://raw.githubusercontent.com/Cumulocity-IoT/cumulocity-dynamic-mapper/refs/heads/main/LICENSE")
+                .license(mitLicense);
 
         return new OpenAPI()
-            .info(info)
-            .externalDocs(new ExternalDocumentation()
-                .description("Additional Documentation")
-                .url("https://cumulocity.com/docs/"));
+                .info(info)
+                .externalDocs(new ExternalDocumentation()
+                        .description("Additional Documentation")
+                        .url("https://cumulocity.com/docs/"))
+                .servers(Collections.emptyList());
     }
 }
