171db926f45387c7b3ee84907f18e500416d0397
ck-c8y <christof.strack@cumulocity.com>
2025-06-05 13:36:02 +0200

    format code

--- dynamic-mapping-service/src/main/java/dynamic/mapping/processor/inbound/CodeBasedProcessorInbound.java
+++ dynamic-mapping-service/src/main/java/dynamic/mapping/processor/inbound/CodeBasedProcessorInbound.java
@@ -87,52 +87,40 @@ public class CodeBasedProcessorInbound extends BaseProcessorInbound<Object> {
 
             String identifier = Mapping.EXTRACT_FROM_SOURCE + "_" + mapping.identifier;
             Value bindings = graalsContext.getBindings("js");
-            //Value extractFromSourceFunc =
-            //        bindings.getMember(identifier);
+            // Value extractFromSourceFunc =
+            // bindings.getMember(identifier);
 
-            //if (extractFromSourceFunc == null) {
-                byte[] decodedBytes = Base64.getDecoder().decode(mapping.code);
-                String decodedCode = new String(decodedBytes);
-                String decodedCodeAdapted = decodedCode.replaceFirst(
-                        Mapping.EXTRACT_FROM_SOURCE,
-                        identifier);
-                Source source = Source.newBuilder("js", decodedCodeAdapted, identifier +
-                                ".js")
-                        .buildLiteral();
-                graalsContext.eval(source);
-                Value extractFromSourceFunc = bindings
-                        .getMember(identifier);
-            //}
+            // if (extractFromSourceFunc == null) {
+            byte[] decodedBytes = Base64.getDecoder().decode(mapping.code);
+            String decodedCode = new String(decodedBytes);
+            String decodedCodeAdapted = decodedCode.replaceFirst(
+                    Mapping.EXTRACT_FROM_SOURCE,
+                    identifier);
+            Source source = Source.newBuilder("js", decodedCodeAdapted, identifier +
+                    ".js")
+                    .buildLiteral();
+            graalsContext.eval(source);
+            Value extractFromSourceFunc = bindings
+                    .getMember(identifier);
+            // }
 
             if (context.getSharedCode() != null) {
-                byte[] decodedSharedCodeBytes =
-                        Base64.getDecoder().decode(context.getSharedCode());
+                byte[] decodedSharedCodeBytes = Base64.getDecoder().decode(context.getSharedCode());
                 String decodedSharedCode = new String(decodedSharedCodeBytes);
                 Source sharedSource = Source.newBuilder("js", decodedSharedCode,
-                                "sharedCode.js")
+                        "sharedCode.js")
                         .buildLiteral();
                 graalsContext.eval(sharedSource);
             }
 
             if (context.getSystemCode() != null) {
-                byte[] decodedSystemCodeBytes =
-                        Base64.getDecoder().decode(context.getSystemCode());
+                byte[] decodedSystemCodeBytes = Base64.getDecoder().decode(context.getSystemCode());
                 String decodedSystemCode = new String(decodedSystemCodeBytes);
                 Source systemSource = Source.newBuilder("js", decodedSystemCode,
-                                "systemCode.js")
+                        "systemCode.js")
                         .buildLiteral();
                 graalsContext.eval(systemSource);
             }
-
-//            if (context.getSystemSource() != null) {
-//                graalsContext.eval(context.getSystemSource());
-//            }
-//            if (context.getSharedSource() != null) {
-//                graalsContext.eval(context.getSharedSource());
-//            }
-//            if (context.getMappingSource() != null) {
-//                graalsContext.eval(context.getMappingSource());
-//            }
 
             Map jsonObject = (Map) context.getPayload();
             String payloadAsString = Functions.string(context.getPayload(), false);
