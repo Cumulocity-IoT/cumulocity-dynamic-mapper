fe93c06a7dc9d2f0348e2c56bacee53d69e570d0
ck-c8y <christof.strack@cumulocity.com>
2025-06-27 13:28:50 +0200

    annotate remaining controller

--- dynamic-mapping-service/src/main/java/dynamic/mapping/controller/DeploymentController.java
+++ dynamic-mapping-service/src/main/java/dynamic/mapping/controller/DeploymentController.java
@@ -33,7 +33,6 @@ import dynamic.mapping.connector.core.registry.ConnectorRegistry;
 import dynamic.mapping.connector.core.registry.ConnectorRegistryException;
 import dynamic.mapping.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
