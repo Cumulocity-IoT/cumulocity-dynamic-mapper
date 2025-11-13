eaa76ad8a3c59b0531457d642c47f5cd7c4f1800
ck-c8y <christof.strack@cumulocity.com>
2025-11-13 16:47:58 +0100

    fix localization

--- dynamic-mapper-ui/src/i18n.ts
+++ dynamic-mapper-ui/src/i18n.ts
@@ -3,4 +3,15 @@
  * You can always add additional strings by adding your own po file. All po files are
  * combined to one JSON file per language and are loaded if the specific language is needed.
  */
-import './locales/de.po'; // <- adding additional strings to the german translation.
+import '@c8y/ngx-components/locales/de.po';
+import '@c8y/ngx-components/locales/en.po';
+import '@c8y/ngx-components/locales/en_US.po';
+import '@c8y/ngx-components/locales/es.po';
+import '@c8y/ngx-components/locales/fr.po';
+import '@c8y/ngx-components/locales/ja_JP.po';
+import '@c8y/ngx-components/locales/ko.po';
+import '@c8y/ngx-components/locales/nl.po';
+import '@c8y/ngx-components/locales/pl.po';
+import '@c8y/ngx-components/locales/pt_BR.po';
+import '@c8y/ngx-components/locales/zh_CN.po';
+import '@c8y/ngx-components/locales/zh_TW.po';
