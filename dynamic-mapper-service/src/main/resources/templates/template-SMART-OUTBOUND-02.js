75aa23a3c1e155bcc2483f5431dea4950d89c563
ck-c8y <christof.strack@cumulocity.com>
2025-11-25 16:34:23 +0100

    test with array

---
+++ dynamic-mapper-service/src/main/resources/templates/template-SMART-OUTBOUND-02.js
@@ -0,0 +1,50 @@
+/**
+ * @name Default template for Smart Function
+ * @description Default template for Smart Function, creates one measurement as array
+ * @templateType OUTBOUND_SMART_FUNCTION
+ * @direction OUTBOUND
+ * @defaultTemplate true
+ * @internal true
+ * @readonly true
+ * 
+*/
+
+function onMessage(msg, context) {
+    var payload = msg.getPayload();
+
+    console.log("Context" + context.getStateAll());
+    console.log("Payload Raw:" + payload);
+    console.log("Payload messageId" +  payload.get('messageId'));
+
+    // use _externalId_ to reference the external id of the device.
+    // it is resolved automatically using the externalId type from externalSource: [{"type":"c8y_Serial"}]
+    // e.g. topic: `measurements/_externalId_`
+    // return [{  
+    //    topic: `measurements/_externalId_`,
+    //    payload: {
+    //        "time":  new Date().toISOString(),
+    //        "c8y_Steam": {
+    //            "Temperature": {
+    //            "unit": "C",
+    //            "value": payload["c8y_TemperatureMeasurement"]["T"]["value"]
+    //            }
+    //        }
+    //    },
+    //    transportFields: { "key": payload["source"]["id"]},  // define key to add to Kafka payload (record)
+    //    externalSource: [{"type":"c8y_Serial"}]
+    // }];
+
+    return [{  
+        topic: `measurements/${payload["source"]["id"]}`,
+        payload:[{
+            "time":  new Date().toISOString(),
+            "c8y_Steam": {
+                "Temperature": {
+                "unit": "C",
+                "value": payload["c8y_TemperatureMeasurement"]["T"]["value"]
+                }
+            }
+        }],
+        transportFields: { "key": payload["source"]["id"]}  // define key to add to Kafka payload (record)
+    }];
+}
