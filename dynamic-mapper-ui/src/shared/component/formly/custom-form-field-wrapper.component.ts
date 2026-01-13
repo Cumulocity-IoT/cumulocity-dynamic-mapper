09fdeee4e3b5f9e3590257625a04d9f68a0b4d7a
ck-c8y <christof.strack@cumulocity.com>
2026-01-13 19:11:04 +0100

    changes

---
+++ dynamic-mapper-ui/src/shared/component/formly/custom-form-field-wrapper.component.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 Cumulocity GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @authors Christof Strack
+ */
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CoreModule, FormsModule } from '@c8y/ngx-components';
+import { FieldWrapper, FormlyModule } from '@ngx-formly/core';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+
+@Component({
+  selector: 'd11r-wrapper-form-field',
+  templateUrl: './custom-form-field-wrapper.component.html',
+  standalone: true,
+  imports: [
+    CommonModule,
+    CoreModule,
+    FormsModule,
+    FormlyModule,
+    PopoverModule
+  ]
+  // changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class WrapperCustomFormField extends FieldWrapper implements OnInit {
+  maxHelpBlockLength = 64;
+  showDescriptionAsPopup: boolean;
+  customWrapperClass: string;
+  classes: string;
+
+  ngOnInit() {
+    this.showDescriptionAsPopup =
+      this.field.type === 'radio' ||
+      this.field.type === 'typeahead' ||
+      (this.props.description &&
+        this.props.description.length > this.maxHelpBlockLength);
+    // Get the custom class from the field's templateOptions
+    this.customWrapperClass = this.field.props?.['customWrapperClass'] || '';
+    this.classes = `form-group ${this.customWrapperClass}`;
+  }
+}
