801442ce2571585c1bc14bacda4cc75ade3843f1
ck-c8y <christof.strack@cumulocity.com>
2026-08-04 16:42:04 +0200

    convert documentation to md format

--- dynamic-mapper-ui/cumulocity.config.ts
+++ dynamic-mapper-ui/cumulocity.config.ts
@@ -8,11 +8,11 @@ export default {
     license,
     version,
     name: 'Dynamic Mapper',
-    contextPath: 'c8y-pkg-dynamic-mapper-dev',
+    contextPath: 'c8y-pkg-dynamic-mapper',
     icon: {
       class: 'c8y-icon-dynamic-mapper'
     },
-    key: 'c8y-pkg-dynamic-mapper-dev-key',
+    key: 'c8y-pkg-dynamic-mapper-key',
     contentSecurityPolicy:
       "base-uri 'none'; default-src 'self' 'unsafe-inline' http: https: ws: wss:; connect-src 'self' http: https: ws: wss:;  script-src 'self' *.bugherd.com *.twitter.com *.twimg.com *.aptrinsic.com 'unsafe-inline' 'unsafe-eval' data:; style-src * 'unsafe-inline' blob:; img-src * data: blob:; font-src * data:; frame-src *; worker-src 'self' blob:;",
     dynamicOptionsUrl: '/apps/public/public-options/options.json',
