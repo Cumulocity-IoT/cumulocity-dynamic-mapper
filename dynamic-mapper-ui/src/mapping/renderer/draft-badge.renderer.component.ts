d819c9092145ca7547ab8ff05f2d6de07be8699d
ck-c8y <christof.strack@cumulocity.com>
2026-08-17 08:04:41 +0200

    new feature #546

---
+++ dynamic-mapper-ui/src/mapping/renderer/draft-badge.renderer.component.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2025 Cumulocity GmbH
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @authors Christof Strack
+ */
+
+import { Component } from '@angular/core';
+import { CellRendererContext, CoreModule } from '@c8y/ngx-components';
+
+@Component({
+  selector: 'd11r-draft-badge-renderer',
+  template: `
+    @if (context.value) {
+      <span class="text-12 label label-info" [attr.data-cy]="'dm-versions-draft-' + context.item.id"
+        title="This mapping has an unpublished draft version">draft</span>
+    }
+  `,
+  standalone: true,
+  imports: [CoreModule]
+})
+export class DraftBadgeRendererComponent {
+  constructor(public readonly context: CellRendererContext) {}
+}
